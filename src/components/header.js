ee14205f7c6e0df85c8d05b28856b1702aa7d118
Ezzdin Atef <ezzdin1125@gmail.com>
2020-07-30 18:15:40 +0200

    add link to the photo

--- src/components/header.js
+++ src/components/header.js
@@ -1,43 +1,56 @@
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import "font-awesome/css/font-awesome.css"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div className="container">
-      <div className="info">
-        <img src="profile.jpeg" alt="" />
-        <div className="text">
-          <Link to="/">{siteTitle}</Link>
-          <p>A Web Developer</p>
+const Header = ({ siteTitle }) => {
+  // const query = graphql`
+  //   query {
+  //     fileName: file(relativePath: { eq: "images/myimage.jpg" }) {
+  //       childImageSharp {
+  //         fluid(maxWidth: 400, maxHeight: 250) {
+  //           ...GatsbyImageSharpFluid
+  //         }
+  //       }
+  //     }
+  //   }
+  // `
+  return (
+    <header>
+      <div className="container">
+        <div className="info">
+          <img src="https://ezzdinatef.me/img/personal.jpeg" alt="" />
+          <div className="text">
+            <Link to="/">{siteTitle}</Link>
+            <p>A Web Developer</p>
+          </div>
+        </div>
+        <div className="social">
+          <ul>
+            <li>
+              <a href="https://twitter.com/ezzdin_atef" target="_blank">
+                <i className="fa fa-twitter" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/channel/UCcV8kzgd-DhVXCUYDb8fGYw"
+                target="_blank"
+              >
+                <i className="fa fa-youtube-play" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://codepen.io/ezzdin_atef" target="_blank">
+                <i className="fa fa-codepen" aria-hidden="true"></i>
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-      <div className="social">
-        <ul>
-          <li>
-            <a href="https://twitter.com/ezzdin_atef" target="_blank">
-              <i className="fa fa-twitter" aria-hidden="true"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.youtube.com/channel/UCcV8kzgd-DhVXCUYDb8fGYw"
-              target="_blank"
-            >
-              <i className="fa fa-youtube-play" aria-hidden="true"></i>
-            </a>
-          </li>
-          <li>
-            <a href="https://codepen.io/ezzdin_atef" target="_blank">
-              <i className="fa fa-codepen" aria-hidden="true"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
